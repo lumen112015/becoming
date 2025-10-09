bfb8f483639da1771d0871dfe680a8cf03fec45b
lumen112015 <becoming.lumen@gmail.com>
2025-10-09 12:59:16 -0500

    turn off trailing slashes

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,6 +6,7 @@ import mdx from "@astrojs/mdx";
 export default defineConfig({
   site: "https://becominglumen.com",
   integrations: [mdx()],
+  trailingSlash: "ignore",
   devToolbar: {
     enabled: false,
   },
